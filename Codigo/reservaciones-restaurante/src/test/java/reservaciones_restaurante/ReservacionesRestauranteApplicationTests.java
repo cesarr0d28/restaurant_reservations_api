d495304b554605d3e84a62a9bde49d4e02360792
Cesar Guevara <cesar_r0d28@hotmail.com>
2024-12-14 11:21:31 -0500

    Se hacen modificaciones en las clases segun las recomendaciones de sonarqube para reducir incidentes de mantenibilidad

--- Codigo/reservaciones-restaurante/src/test/java/reservaciones_restaurante/ReservacionesRestauranteApplicationTests.java
+++ Codigo/reservaciones-restaurante/src/test/java/reservaciones_restaurante/ReservacionesRestauranteApplicationTests.java
@@ -1,15 +1,22 @@
 package reservaciones_restaurante;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.reservaciones_restaurante.ReservacionesRestauranteApplication;
 
 @SpringBootTest(classes = ReservacionesRestauranteApplication.class)
-public class ReservacionesRestauranteApplicationTests {
+class ReservacionesRestauranteApplicationTests {
+
+    @Autowired
+    private ReservacionesRestauranteApplication application;
 
     @Test
     void contextLoads() {
-        // Este test verifica que el contexto se cargue correctamente
+        // Verificar que el contexto se cargue correctamente
+        assertThat(application).isNotNull();
     }
 }
